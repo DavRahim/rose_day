52afddb50e7c1698b2b02a524609ca107d39d62e
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-17 00:50:52 +0600

    hero work done

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,6 +5,7 @@ import { IoMdHeartEmpty } from "react-icons/io";
 import { TbShoppingCart } from "react-icons/tb";
 import Image from "next/image";
 import logo from "../../public/logo.png"
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 type Props = {};
 
@@ -12,14 +13,22 @@ const Header = (props: Props) => {
   const activeItem = 1
   return (
     <div className="w-full mx-auto bg-primary">
-      <div className="1440px:w-[1150px] mx-auto py-3 flex items-center justify-between">
+      <div className="800px:w-[1150px] w-full mx-auto py-3 flex items-center justify-between">
         <NavItem activeItem={activeItem} isMobile={false} />
+        <div className='800px:hidden'>
+          <HiOutlineMenuAlt3
+            size={25}
+            className='cursor-pointer dark:text-white text-black'
+          // onClick={() => setOpenSidebar(true)}
+          />
+        </div>
         <Image
           alt="logo"
           src={logo}
           width={119}
           height={86}
         />
+       
         <div className="flex items-center gap-2">
           <IoPersonOutline size={20} color="white"/>
           <IoSearch size={20} color="white"/>
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,24 +12,8 @@ import hero4 from "../../public/pattern.png";
 const Hero = (props: Props) => {
     return (
         <div className="bg-primary">
-            <div className="1440px:w-[1150px] pb-20 mx-auto flex justify-between items-center">
-                <div className="">
-                    <h1 className="text-[64px] text-secondary font-medium font-leagueSpartan leading-[72px]">Happiness <br /> blooms from <br /> within</h1>
-                    <p className="text-[16px] text-secondary py-5">Our environment, the world in which we live and <br />work, is a mirror of our attitudes and expectations.</p>
-
-                    <div className="flex items-center gap-3">
-                        <button className="bg-secondary text-primary text-[16px] text-center w-[156px] h-[59px] mr-5">
-                            Shop now
-                        </button>
-                        <button className="text-[16px] text-secondary flex gap-2 items-center">
-                            <p> Explore plants</p>
-                            <FaArrowRightLong />
-                        </button>
-
-                    </div>
-                </div>
-
-                <div className="flex gap-1">
+            <div className="800px:w-[1150px] w-full 800px:flex-row flex-col pb-20 mx-auto flex justify-between items-center">
+                <div className="flex gap-1 800px:order-last">
                     <div>
                         <Image
                             src={hero1}
@@ -68,14 +52,28 @@ const Hero = (props: Props) => {
                             <div className="flex flex-col justify-end h-full">
                                 <h1 className="text-[28px] text-secondary">Anthurium Flower</h1>
                                 <p className="text-[14px] text-secondary my-[18px]">The flower of human being. It has meaningful of fact that the plant always grow whatever season and weather...</p>
-                                <button className="w-[327px] h-[48px] mb-2 bg-secondary text-primary">
+                                <button className="w-[100%] h-[48px] mb-2 bg-secondary text-primary">
                                     READ MORE
                                 </button>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div className="">
+                    <h1 className="text-[64px] text-secondary font-medium font-leagueSpartan leading-[72px]">Happiness <br /> blooms from <br /> within</h1>
+                    <p className="text-[16px] text-secondary py-5">Our environment, the world in which we live and <br />work, is a mirror of our attitudes and expectations.</p>
 
+                    <div className="flex items-center gap-3">
+                        <button className="bg-secondary text-primary text-[16px] text-center w-[156px] h-[59px] mr-5">
+                            Shop now
+                        </button>
+                        <button className="text-[16px] text-secondary flex gap-2 items-center">
+                            <p> Explore plants</p>
+                            <FaArrowRightLong />
+                        </button>
+
+                    </div>
+                </div>
             </div>
         </div>
 
