68464c2a5b4189f487e6d6d168c2ee36a766b8a6
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-17 09:51:43 +0600

    reade me update

--- app/utils/NavItem.tsx
+++ app/utils/NavItem.tsx
@@ -28,17 +28,20 @@ type Props = {
 }
 const NavItem: React.FC<Props> = ({ activeItem, isMobile }) => {
 
-    return (<>
-        <div className="hidden 800px:flex">
-            {
-                navItemData && navItemData.map((item, index) => (
-                    <Link href={`${item.url}`} key={index} passHref>
-                        <span className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-secondary"}  text-[16px] px-6 font-leagueSpartan font-[400]`}>{item.name}</span>
-                    </Link>
-                ))
-            }
-        </div>
-    </>);
+    return (
+        <>
+            <div className="hidden 800px:flex">
+                {
+                    navItemData && navItemData.map((item, index) => (
+                        <Link href={`${item.url}`} key={index} passHref>
+                            <span className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-secondary"}  text-[16px] px-6 font-leagueSpartan font-[400]`}>{item.name}</span>
+                        </Link>
+                    ))
+                }
+            </div>
+        </>
+
+    );
 };
 
 export default NavItem;
