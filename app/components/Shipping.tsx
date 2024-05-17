baac1057dc3c14e178b659839ff2dddcd9db3b5b
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-18 00:51:10 +0600

    some feature add

--- app/components/Shipping.tsx
+++ app/components/Shipping.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import React from "react";
+import { IoCallOutline } from "react-icons/io5";
+import { MdOutlineMailOutline } from "react-icons/md";
 import shipping from "../../public/image13.png"
 
 type Props = {};
@@ -8,13 +10,21 @@ const Shipping = (props: Props) => {
   return (
       <div className="w-full mx-auto mb-10">
           <div className="800px:w-[1150px] w-full mx-auto">
-            <div>
+            <div className="relative">
                 <Image
                 src={shipping}
                 alt=""
                 width={1150}
                 height={346}
                 />
+                <div>
+                    <h1>
+                          Free Shipping Services
+                    </h1>
+                    <p>*only for the same region</p>
+                      <p><span><IoCallOutline /></span> <span>+62 1189-2719-00</span></p>
+                      <p><span><MdOutlineMailOutline /></span> <span>order@platify.co</span></p>
+                </div>
             </div>
 
           </div>
